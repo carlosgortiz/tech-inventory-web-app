1d59bc39ccf395919ce59f6a86dab988eba8dfce
dorecio <dorisrecio@gmail.com>
2023-01-03 21:25:15 -0600

    se agrega archivo server.js

---
+++ server.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const path = require('path');
+const express = require('express');
+//const session = require('express-session');
+//const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
+//const routes = require('./controllers');
+//const helpers = require('./utils/helpers');
+const app = express();
+const PORT = process.env.PORT || 3001;
+/*
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+app.use(session(sess));
+*/
+const hbs = exphbs.create({});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+//app.use(routes);
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(` Now listening on port ${PORT} `));
+});

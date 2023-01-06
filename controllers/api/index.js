f1309c5611bfb5e26b219643327cb80079912910
dorecio <dorisrecio@gmail.com>
2023-01-05 20:15:50 -0600

    se actualiza carpeta de rutas

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,14 +1,9 @@
 const router = require('express').Router();
-<<<<<<< HEAD
-
-const softwareRoutes = require('./software-routes');
-router.use('/software', softwareRoutes);
-
-module.exports = router;
-=======
 const hardwareRoutes = require('./hardwareRoutes');
+const softwareRoutes = require('./softwareRoutes');
+
 
 router.use('/hardwares', hardwareRoutes);
+router.use('/softwares', softwareRoutes);
 
 module.exports = router;
->>>>>>> 484d1b5a596d6ad8db4eca4911d04e5d9c3320c8
--- schema/schema.sql
+++ schema/schema.sql
@@ -1,2 +1,3 @@
 DROP DATABASE IF EXISTS tech_inv_web_app_db;
 CREATE DATABASE tech_inv_web_app_db;
+

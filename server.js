6fd7c9fd546fa844f24e2f7d5d1d334521e38aeb
Julio C. MW11 <sysa.mail@gmail.com>
2023-01-09 10:04:26 -0600

    se agrega registro, se resuelve issue#46

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 require('dotenv').config();
 const path = require('path');
 const express = require('express');
-//const session = require('express-session');
-//const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 //const helpers = require('./utils/helpers');
 const app = express();
 const PORT = process.env.PORT || 3001;
-/*
+
+
 const sess = {
     secret: 'Super secret secret',
-    cookie: {},
+    cookie: {
+      // Stored in milliseconds (86400000 === 1 day)
+      maxAge: 86400,
+      secure: false,        
+    },
     resave: false,
     saveUninitialized: true,
     store: new SequelizeStore({
@@ -20,7 +25,7 @@ const sess = {
     })
 };
 app.use(session(sess));
-*/
+
 const hbs = exphbs.create({});
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
